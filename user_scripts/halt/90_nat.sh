07b5fa9a48aba9553dc0b587194ff5f43a7ad826
Hoang Tran <tranviet@uclouvain.be>
2016-12-13 17:45:01 +0100

    enable MPTCP over multiple interfaces
    Goal: to allow guest to use both wlan0 and eth0 of host for MPTCP
    to outside world.
    This requires to set source-routing on both host and guest.

--- user_scripts/halt/90_nat.sh
+++ user_scripts/halt/90_nat.sh
@@ -4,13 +4,24 @@ set -x
 echo "port halt nat"
 uname_str=$(uname)
 
-echo "==> Disabling IP Masquerading"
+read interface2 ip2 gateway2 < ./second_interface.txt
+
 if [[ "$uname_str" == "Linux" ]]; then
+
+	echo "==> Remove source-routing"
+
+	sudo ip rule del from 192.168.34.10 table 2
+	sudo ip route del 192.168.34.0/24 dev $interface2 scope link table 2
+	sudo ip route del default  via $gateway2  dev $interface2    table 2
+
+	echo "==> Disabling IP Masquerading on host"
+
 	sudo iptables -t nat -D POSTROUTING -s 192.168.33.0/24 -j MASQUERADE
+
+	sudo iptables -t nat -D POSTROUTING -s 192.168.34.0/24 -j MASQUERADE -o $interface2
+
 elif [[ "$uname_str" == "Darwin" ]]; then
 	sudo pfctl -df /etc/pf.conf > /dev/null 2>&1;
 else
 	echo "FAILED! Host OS unknown"
 fi
-
-
--- user_scripts/up/10_nat.sh
+++ user_scripts/up/10_nat.sh
@@ -4,12 +4,24 @@ set -x
 echo "port up nat"
 uname_str=$(uname)
 
-echo "==> Enabling IP Masquerading"
+read interface2 ip2 gateway2 < ./second_interface.txt
+
 if [[ "$uname_str" == "Linux" ]]; then
+
+	echo "==> Set source-routing"
+	# forward packets from 192.168.34.10 to second interface
+	sudo ip rule add from 192.168.34.10 table 2
+	sudo ip route add 192.168.34.0/24 dev $interface2 scope link table 2
+	sudo ip route add default  via $gateway2  dev $interface2    table 2
+
+	echo "==> Enabling IP Masquerading on host"
+	# set masquerade on default interface
 	sudo iptables -t nat -A POSTROUTING -s 192.168.33.0/24 -j MASQUERADE
+	# set masquerade on second interface
+	sudo iptables -t nat -A POSTROUTING -s 192.168.34.0/24 -j MASQUERADE -o $interface2
+
 elif [[ "$uname_str" == "Darwin" ]]; then
 	echo "nat on en0 from 192.168.33.0/24 to any -> en0" | sudo pfctl -ef - >/dev/null 2>&1; 
 else
 	echo "FAILED! Host OS unknown"
 fi
-
