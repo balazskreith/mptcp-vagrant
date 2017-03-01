ac023744c444df3c19fb93ff8dbe12285b2bfa11
Hoang Tran <hoang.tran@uclouvain.be>
2017-03-01 16:51:14 +0100

    auto update, compile and install iperf3

--- scripts/update_iperf_test_scripts.sh
+++ scripts/update_iperf_test_scripts.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-# update iperf_test_scripts from bitbucket
-# this is similar to "git pull"
-# but can avoid merge conflict if we change git history
+echo "update iperf_test_scripts"
+# this is similar to "git pull",
+# but can avoid merge conflict if we change git history.
+# Note that "origin-ssh" is the same remote repo as "origin",
+# but the url type is 'ssh' instead of 'https'
+
 cd /home/vagrant/
 git fetch origin-ssh
 git reset --hard origin-ssh/master
+
+echo "update latest iperf3"
+cd iperf/
+git fetch origin-ssh
+git reset --hard origin-ssh/mptcp-test
+
+echo "compile and install iperf3"
+make -j4  		   >/dev/null 2>&1
+sudo make install  >/dev/null 2>&1
